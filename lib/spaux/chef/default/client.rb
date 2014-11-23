2b4dbf588b89e77b5e1394d308e98d82d8169f5a
Miguel Landaeta <miguel@miguel.cc>
2014-11-23 18:44:17 -0300

    Set no_lazy_load true by default for chef-client
    https://sethvargo.com/using-gems-with-chef/#comment-1698022542

--- lib/spaux/chef/default/client.rb
+++ lib/spaux/chef/default/client.rb
@@ -4,5 +4,6 @@ client_key       ::File.join('@work_dir', 'client.pem')
 json_attribs     ::File.join('@work_dir', 'attributes.json')
 chef_server_url  'https://api.opscode.com/organizations/spaux'
 ssl_verify_mode  :verify_peer
+no_lazy_load     true
 node_name        'spaux'
 override_runlist ["recipe[spaux::machine]"]

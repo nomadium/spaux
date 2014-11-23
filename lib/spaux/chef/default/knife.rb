b667c83ce90e6e2929bc4fae6c5a3f0f13b22f16
Miguel Landaeta <miguel@miguel.cc>
2014-11-23 20:40:35 -0300

    Remove wrong cookbook_path default and fix issue #6

--- lib/spaux/chef/default/knife.rb
+++ lib/spaux/chef/default/knife.rb
@@ -2,5 +2,4 @@ current_dir = File.dirname(__FILE__)
 node_name                "spaux"
 client_key               ::File.join(current_dir, 'spaux.pem')
 chef_server_url          "https://api.opscode.com/organizations/spaux"
-cookbook_path            [current_dir]
 ssl_verify_mode          :verify_peer

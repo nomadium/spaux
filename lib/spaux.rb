8b07f4e7f2341591927325369204718e8cc606c1
Miguel Landaeta <miguel@miguel.cc>
2014-11-23 15:56:43 -0300

    Rename default knife.rb config file

--- lib/spaux.rb
+++ lib/spaux.rb
@@ -6,7 +6,7 @@ require 'spaux/chef/knife'
 class Spaux
   def self.default_knife_config
     lib_dir = ::File.expand_path(::File::join(__FILE__, '..'))
-    knife_rb = ::File::join(lib_dir, 'spaux', 'defaults', 'knife.rb')
+    knife_rb = ::File::join(lib_dir, 'spaux', 'default_knife.rb')
     ::Chef::Config.from_string(::File.read(knife_rb), knife_rb)
     ::Chef::Config.configuration
   end
